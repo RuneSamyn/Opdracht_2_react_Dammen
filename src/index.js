fa79f03e6fd220cde2d446484c2b73180a70474d
rune samyn <rune.samyn@gmail.com>
2020-11-04 16:59:37 +0100

    make 10x10 play grid + add startpositions for pieces

--- src/index.js
+++ src/index.js
@@ -5,55 +5,103 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 class Board extends React.Component {
-    renderRow(i) {
-        return <Row value={i} />;
+  constructor(props) {
+    super(props);
+    var board = Array(100).fill("x");
+    for (var i = 0; i < 20; i++) {
+      var x = 2*i;
+      if((x >= 10 && x < 20) || (x >= 30 && x < 40) )
+        x++;
+      board[x] = "w";
+      board[99-x] = "z";
     }
-    render() {
-        const status = 'Next player: X';
-    
-        return (
-          <div>
-            <div className="status">{status}</div>
-            <div className="row mb-5">
-              {this.renderRow(0)}
-              {this.renderRow(1)}
-              {this.renderRow(2)}
-              {this.renderRow(3)}
-              {this.renderRow(4)}
-              {this.renderRow(5)}
-              {this.renderRow(6)}
-            </div>
-          </div>
-        );
-      }
+    this.state = {
+      gameBoard: board
+    }
+    console.log(this.state.gameBoard)
+  }
+
+  renderLine(i, array) {
+      return <Line 
+        value={i}
+        array={array}
+      />;
+  }
+
+  render() {
+      const status = 'Next player: X';
+  
+      return (
+      <div className="">
+        <div className="status">{status}</div>
+        {this.renderLine(0, this.state.gameBoard.slice(0, 10))}
+        {this.renderLine(10, this.state.gameBoard.slice(10, 20))}
+        {this.renderLine(20, this.state.gameBoard.slice(20, 30))}
+        {this.renderLine(30, this.state.gameBoard.slice(30, 40))}
+        {this.renderLine(40, this.state.gameBoard.slice(40, 50))}
+        {this.renderLine(50, this.state.gameBoard.slice(50, 60))}
+        {this.renderLine(60, this.state.gameBoard.slice(60, 70))}
+        {this.renderLine(70, this.state.gameBoard.slice(70, 80))}
+        {this.renderLine(80, this.state.gameBoard.slice(80, 90))}
+        {this.renderLine(90, this.state.gameBoard.slice(90, 100))}
+      </div>
+      );
+    }
 }
 
-class Row extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />;
-    }
-    render() {
-        return (
-            <button className="col h-75">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
-            </button>
-        )
-    }
+class Line extends React.Component {
+  renderSquare(i, p) {
+    return <Square 
+      value={i}
+      pion={p} 
+    />;
+  }
+  render() {
+      return (
+        <div className="col-container">
+            {this.renderSquare(0 + this.props.value, this.props.array[0])}
+            {this.renderSquare(1 + this.props.value, this.props.array[1])}
+            {this.renderSquare(2 + this.props.value, this.props.array[2])}
+            {this.renderSquare(3 + this.props.value, this.props.array[3])}
+            {this.renderSquare(4 + this.props.value, this.props.array[4])}
+            {this.renderSquare(5 + this.props.value, this.props.array[5])}
+            {this.renderSquare(6 + this.props.value, this.props.array[6])}
+            {this.renderSquare(7 + this.props.value, this.props.array[7])}
+            {this.renderSquare(8 + this.props.value, this.props.array[8])}
+            {this.renderSquare(9 + this.props.value, this.props.array[9])}
+          </div>
+      )
+  }
 }
 
 class Square extends React.Component {
     render() {
         return (
-        <div className="square inline-block">
-            {this.props.value}
-        </div>
-    )
-}
+          <button className={"square col" +(this.isBrown() ? " brown" : " white")}>
+              <img className="pion" src={this.imageUri(this.props.pion)}></img>
+          </button>
+        )
+    }
+    imageUri(pion) {
+      if(pion === "w") {
+        return "/images/wit.png";
+      }
+      else if(pion === "wd") {
+        return "/images/witD.png";
+      }
+      else if(pion === "z") {
+        return "/images/zwart.png";
+      }
+      else if(pion === "zd") {
+        return "/images/zwartD.png";
+      }
+    }
+
+    isBrown() {
+      var evenCol = this.props.value % 2;
+      var evenLine = ((this.props.value - this.props.value % 10) / 10) %2
+      return evenLine ? evenCol : !evenCol;
+    }
 }
 
 class Game extends React.Component {
