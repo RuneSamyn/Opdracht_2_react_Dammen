36d896e4db75015f30ce3bf6c72fe79d0401a930
rune samyn <rune.samyn@gmail.com>
2020-11-04 11:01:25 +0100

    delete all example code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+class Board extends React.Component {
+    renderRow(i) {
+        return <Row value={i} />;
+    }
+    render() {
+        const status = 'Next player: X';
+    
+        return (
+          <div>
+            <div className="status">{status}</div>
+            <div className="row mb-5">
+              {this.renderRow(0)}
+              {this.renderRow(1)}
+              {this.renderRow(2)}
+              {this.renderRow(3)}
+              {this.renderRow(4)}
+              {this.renderRow(5)}
+              {this.renderRow(6)}
+            </div>
+          </div>
+        );
+      }
+}
+
+class Row extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+    render() {
+        return (
+            <button className="col h-75">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </button>
+        )
+    }
+}
+
+class Square extends React.Component {
+    render() {
+        return (
+        <div className="square inline-block">
+            {this.props.value}
+        </div>
+    )
+}
+}
+
+class Game extends React.Component {
+    render() {
+      return (
+        <div className="game container">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
